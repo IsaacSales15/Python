102cd8820ea40db7fc32fdd133bc6cf2c448408d
Isaac Sales R. Gomes <124395470+IsaacSales15@users.noreply.github.com>
2023-10-30 20:04:15 -0300

    Calculadora de P.A - Python

---
+++ calculadora_PA.py
@@ -0,0 +1,20 @@
+t1 = int(input("Coloque o primeiro termo da P.A: "))
+t2 = int(input("Coloque o segundo termo da P.A: "))
+
+r = t2 - t1
+
+def calcular_Termos(t1, r):
+    for i in range(10):
+        f = t1 + i * r
+        print(f)
+    
+if r < 0:
+    print(f"A sua P.A é DECRESCENTE e os 10 primeiros termos são: ")
+    calcular_Termos(t1, r)
+    
+elif r > 0:
+    print(f"A sua P.A é CRESCENTE e os 10 primeiros termos são: ")
+    calcular_Termos(t1, r)
+
+else:
+    print("Razão igual a 0!")
